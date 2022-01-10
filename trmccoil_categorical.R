bdb3714eda5c29397a9e1a39feb3af6e9b701fd1
anto @ aleph <trimarsanto@gmail.com>
2022-01-10 20:30:58 +0700

    add options for threshold_site, totalrun and burnin

--- trmccoil_categorical.R
+++ trmccoil_categorical.R
@@ -6,7 +6,16 @@ option.list <- list(
     make_option(c("-o", "--outfile"),
                 help="output filename"),
     make_option(c("-l", "--libpath"),
-                help="library path to THEREALMcCOIL.")
+                help="library path to THEREALMcCOIL."),
+    make_option("--threshold_site",
+                help="threshold_site",
+                default=20),
+    make_option("--totalrun",
+                help="total run",
+                default=10000),
+    make_option("--burnin",
+                help="burn in",
+                default=2000)
 )
 
 parser <- OptionParser(usage="%prog [options] inputfile",
@@ -30,11 +39,14 @@ source(source_file)
 infile = args$args
 outfile = args$options$outfile
 
-warning("Reading from ", infile, "and writing to ", outfile)
+warning("Reading from ", infile, " and writing to ", outfile)
 
 data0=read.table(infile, head=T)
 data=data0[,-1]
 rownames(data)=data0[,1]
-McCOIL_categorical(data,maxCOI=25, threshold_ind=20, threshold_site=20, totalrun=1000, burnin=100,
+McCOIL_categorical(data,maxCOI=25, threshold_ind=20, threshold_site=args$options$threshold_site,
+                   totalrun=args$options$totalrun, burnin=args$options$burnin,
                    M0=15, e1=0.05, e2=0.05, err_method=3,
                    path=libpath, output=outfile)
+
+# EOF
